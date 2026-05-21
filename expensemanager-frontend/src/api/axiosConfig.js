56dfaaa6d651efcf9db174ef28d9610e9eabeba3
Risham Kumar <rishamwork.18@gmail.com>
2026-05-21 23:48:56 +0530

    Refactor axios configuration to standardize API variable naming and simplify request interceptor

--- expensemanager-frontend/src/api/axiosConfig.js
+++ expensemanager-frontend/src/api/axiosConfig.js
@@ -1,20 +1,17 @@
 import axios from "axios";
 
-const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:8080/api",
+const API = axios.create({
+  baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:8080",
 });
 
-api.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem("token");
+API.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
 
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
 
-    return config;
-  },
-  (error) => Promise.reject(error)
-);
+  return config;
+});
 
-export default api;
+export default API;
